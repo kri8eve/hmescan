cf8064ba9b345a855c15d0a1e0f6165e52f2064c
Krishnendu Mondal <mr.kri8eve@gmail.com>
2022-08-31 19:33:08 +0530

    first commit

---
+++ client/src/components/QRScanner.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import QrReader from 'react-qr-scanner'
+export default function QRScanner() {
+    const [result, setresult] = useState('')
+    const [isScanned, setisScanned] = useState(false)
+    function handleScan(data) {
+        if(data!=null){
+            console.log(data)
+            setresult(data.text)
+            setisScanned(true)
+        }
+    }
+    function handleError(err) {
+        console.error(err)
+    }
+    const previewStyle = {
+        height: 240,
+        width: 320,
+    }
+    return (
+        <div>
+            <p>QRScanner</p>
+            {!isScanned ?
+                <QrReader
+                    delay={2000}
+                    style={previewStyle}
+                    onError={handleError}
+                    onScan={handleScan}
+                />
+                :
+                <p>{result}</p>
+        }
+
+           
+        </div>
+    )
+}
